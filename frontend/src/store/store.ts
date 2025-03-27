be83fefc1f7a2d1f501cd2b5fa7677e40d2b2ea9
IrinaTimoshina <106275489+IrinaTimoshina@users.noreply.github.com>
2025-03-27 11:11:32 +0300

    feat(Components/ThemToggle & theme): добавление redux контекста и компоненты для изменения темы & добавление основных цветов темы

---
+++ frontend/src/store/store.ts
@@ -0,0 +1,44 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+// Импорт редьюсеров
+import themeReducer from "./themeSlice";
+
+// Конфигурация redux-persist
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  theme: themeReducer, // Добавляем тему в хранилище
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Создаем Redux Store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }), // thunk уже включен в getDefaultMiddleware()
+});
+
+export const persistor = persistStore(store);
+
+// Типы для Redux
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
