32ac9f6f7ac5d5d4f3bd34be2223a1f38b3f44eb
IrinaTimoshina <106275489+IrinaTimoshina@users.noreply.github.com>
2025-04-05 11:30:47 +0300

    chore(frontend): обновление конфигурационных файлов и docker

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export',
+const nextConfig = {
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
